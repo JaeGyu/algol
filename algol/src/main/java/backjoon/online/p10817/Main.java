401d9a89130f5a5a4d17e7486b685ac7efe4e84d
jpmlyr <jpmlyr@gmail.com>
2016-02-23 22:02:48 +0900

    10817 풀이

---
+++ algol/src/main/java/backjoon/online/p10817/Main.java
@@ -0,0 +1,20 @@
+package backjoon.online.p10817;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] nums = new int[3];
+
+		nums[0] = sc.nextInt();
+		nums[1] = sc.nextInt();
+		nums[2] = sc.nextInt();
+
+		Arrays.sort(nums);
+
+		System.out.println(nums[1]);
+	}
+}
