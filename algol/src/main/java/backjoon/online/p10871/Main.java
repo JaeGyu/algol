e71047fe3c46200a75acc9fe351ef877b7195cff
jpmlyr <jpmlyr@gmail.com>
2016-02-23 22:22:52 +0900

    10871문제 런타임에러 Scanner을 두번 사용하면 런타임에러 발생 하는 것 같음

---
+++ algol/src/main/java/backjoon/online/p10871/Main.java
@@ -0,0 +1,30 @@
+package backjoon.online.p10871;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+
+		int[] scNum = new int[2];
+		scNum[0] = sc1.nextInt();
+		scNum[1] = sc1.nextInt();
+
+		int[] arrNum = new int[scNum[0]];
+
+		for (int i = 0; i < scNum[0]; i++) {
+			arrNum[i] = sc2.nextInt();
+		}
+
+		String result = "";
+
+		for (int a : arrNum) {
+			if (a < scNum[1]) {
+				result += a + " ";
+			}
+		}
+
+		System.out.println(result.trim());
+	}
+}
