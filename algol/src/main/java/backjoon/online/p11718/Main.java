ce802043eb059c61481d286ae49a21257ee9f04d
jpmlyr <jpmlyr@gmail.com>
2016-02-27 22:14:40 +0900

    런타임 에러

--- algol/src/main/java/backjoon/online/p11718/Main.java
+++ algol/src/main/java/backjoon/online/p11718/Main.java
@@ -4,10 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-		
-		String input = sc.next();
-		
-		System.out.println(input);
+		Scanner sc = new Scanner(System.in);
+
+		String result = "";
+
+		for (int i = 0; i < 100; i++) {
+
+			String temp = sc.nextLine();
+			if (temp.isEmpty()) {
+				result += temp;
+				break;
+			} else {
+				result += temp + "\n";
+			}
+		}
+
+		System.out.printf(result);
 	}
 }
